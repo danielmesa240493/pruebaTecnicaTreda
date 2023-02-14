a75e0319e25869886e04da91aee30bf1f686b9ab
danielmesa240493 <danielmesa240493@correo.itm.edu.co>
2023-02-14 12:22:39 -0500

    pruebalogica1

---
+++ pruebalogica1.php
@@ -0,0 +1,13 @@
+<?php
+    class PruebaLogica1{
+      public function multiplos($n){
+        $suma = 0;
+        for ($i = 1; $i < $n; $i++) {
+          if ($i % 3 == 0 || $i % 5 == 0) {
+            $suma += $i;
+          }
+        }
+        return $suma;
+      }
+    }
+?>  
